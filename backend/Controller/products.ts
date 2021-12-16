d9804f556d5675100de537f94727fac14b84a374
Daniele Sarnari <daniele.sarnari@heply.it>
2021-12-16 13:15:09 +0100

    feat: pagination

--- backend/Controller/products.ts
+++ backend/Controller/products.ts
@@ -10,18 +10,43 @@ const app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.set('dataset', dataset())
+app.use(paginate.middleware(100, 100))
 
-app.use(paginate.middleware(10, 50))
+app.use((req, res, next) => {
+  // set default or minimum is 10 (as it was prior to v0.2.0)
+  if (req.query.limit <= 100) req.query.limit = 100
+  next()
+})
 
 //  function for getting all products
 app.get('/', async (req, res) => {
-  try {
-    return res.status(200).send(req.app.get('dataset'))
-  } catch (error) {
-    //  handle errors here
-    console.log(error, 'error!!')
-  }
+  let dataset = req.app.get('dataset')
+
+  const {
+    limit,
+    page
+  } = req.query
+
+  const pageCount = Math.ceil(dataset.length / limit)
+
+  if (page === 1) { dataset = dataset.slice(0, limit) } else { dataset = dataset.slice((page - 1) * limit, page * limit) }
+
+  return res.status(200).send({
+    data: dataset,
+    ...(page > 1 ? { previous: page - 1 } : {}),
+    ...(paginate.hasNextPages(req)(pageCount) === true ? { next: page + 1 } : {})
+  })
 })
 
-module.exports.handler = serverless(app)
+// module.exports.handler = serverless(app)
+
+const handler = serverless(app)
+module.exports.handler = async (event, context) => {
+  const datasetResult = await dataset()
+  app.set('dataset', datasetResult)
+
+  // you can do other things here
+  const result = await handler(event, context)
+  // and here
+  return result
+}
--- backend/dataset/index.ts
+++ backend/dataset/index.ts
@@ -1,31 +1,24 @@
-import { createReadStream } from 'fs'
+import { open } from 'fs/promises'
 import * as path from 'path'
 import { parse } from 'csv-parse/sync'
 
 const filePath = path.join(__dirname, '/flyers_data.csv')
 
 const fetchDataset = async () => {
-  // const originalContent = await readFileSync(
-  //   path.join(__dirname, '/dataset/flyers_data.csv'),
-  //   'utf8'
-  // )
+  const file = await open(filePath, 'r')
+  const readableStream = file.createReadStream({ start: 0, encoding: 'utf8' })
+  let result = []
 
-  // const parsedContent = parse(originalContent, {
-  //   columns: true,
-  //   skip_empty_lines: true
-  // })
+  await (async function () {
+    for await (const chunk of readableStream) {
+      result = parse(chunk, {
+        columns: true,
+        skip_empty_lines: true
+      })
+    }
+  })()
 
-  // return parsedContent
-
-  const readableStream = createReadStream(filePath, 'utf8')
-
-  readableStream.on('error', function (error) {
-    console.log(`error: ${error.message}`)
-  })
-
-  readableStream.on('data', (chunk) => {
-    console.log(chunk)
-  })
+  return result
 }
 
 export default fetchDataset
