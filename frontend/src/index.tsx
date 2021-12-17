05d5963b3a59ee7e6529ac7e68d1c5f5247f483a
Daniele Sarnari <daniele.sarnari@heply.it>
2021-12-17 10:59:51 +0100

    feat: frontend simplified

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
-import App from './components/App'
+import DashboardContent from './components/Dashboard'
 import reportWebVitals from './reportWebVitals'
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <DashboardContent />
   </React.StrictMode>,
   document.getElementById('root')
 )
